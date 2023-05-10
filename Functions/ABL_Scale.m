cb33cdc441c21350eb5760f54eae5a3124e23c33
Ricky Pimentel <44686559+peruvianox@users.noreply.github.com>
2023-05-10 17:03:28 -0400

    Updated scaling
    removed marker scale errors, added scale factor saving into "Subjects" structure

--- Functions/ABL_Scale.m
+++ Functions/ABL_Scale.m
@@ -3,20 +3,26 @@ function[Subjects] = ABL_Scale(Settings, Subjects)
 % Batch process OpenSim Scaling
 
 %% Settings
+import org.opensim.modeling.*
+import java.io.*
+
 GenericFilePath = Settings.GenericPath;
 GenericDir = Settings.GenericDir;
 
 %% Subject Loop
+clc;
 for subj = 1:length(Subjects)
-    
+
     %% Create scale folder and copy over TRC and GRF files
     % create scale folder
     ScaleFolder = strcat(Subjects(subj).Folders.OpenSimFolder, '\ScaleFiles');
     mkdir(ScaleFolder);
     StaticTrials = strcmp({Subjects(subj).Trials.type}, 'static');
-    
+    disp(strcat('Scaling ', Subjects(subj).name));
+    cd(ScaleFolder);
+
     % make virtual markers and copy into scale folder
-    useHJC = 1;
+    useHJC = 0;
     if useHJC == 1
         StaticMarkerFile = strcat(Subjects(subj).Folders.path, '\HJC\', ...
             Subjects(subj).Trials(StaticTrials).files.TRC(1:end-4),'_hjc.trc');
@@ -26,10 +32,7 @@ for subj = 1:length(Subjects)
     end
     StaticVirtualFile = [ScaleFolder '\' Subjects(subj).Trials(StaticTrials).files.OpenSimTRC(1:end-4) '_Virtual.trc'];
     Osim.MakeVirtualMkr(StaticMarkerFile, StaticVirtualFile);
-    
-    disp(strcat('Scaling ', Subjects(subj).name));
-    cd(ScaleFolder);
-    
+
     % add mass if not defined
     if isempty(Subjects(subj).Demo)
         fn = [Subjects(subj).name '_Info.csv'];
@@ -37,103 +40,110 @@ for subj = 1:length(Subjects)
         r = contains(T{:,1}, 'Mass');
         Subjects(subj).Demo.mass = str2double(char(T{r, 2}));
     end
-    
-    
+
+
     %% Prep To Scale - Define inputs & outputs
-    import org.opensim.modeling.*
-    import java.io.*
-    
     % define and copy original model OSIM file
-    Orig.Model = strcat(GenericFilePath, '\', GenericDir(contains({GenericDir.name},...
-        'gait2392_Scale_ABLMarkerSet_UMBprobed_locked.osim')).name);
-    SubjModel = strcat(ScaleFolder, '\gait2392_Scale_ABLMarkerSet_UMBprobed_locked.osim');
-    copyfile(Orig.Model, SubjModel);
-    
+    if strcmp(Settings.Model, 'exo')
+        modelName = 'gait2392_ABL_probed_locked_exo.osim';
+    elseif strcmp(Settings.Model, 'generic')
+        modelName = 'gait2392_Scale_ABLMarkerSet_UMBprobed_locked.osim';
+    end
+    if strcmp(Settings.CustomModel, 'Yes') % use customized model
+        SubjModel = strcat(ScaleFolder, '\', modelName);
+    else % or copy from generic
+        Orig.Model = strcat(GenericFilePath, '\', GenericDir(contains({GenericDir.name},modelName)).name);
+        SubjModel = strcat(ScaleFolder, '\', modelName);
+        copyfile(Orig.Model, SubjModel);
+    end
+
     % Load the model and initialize
-    model = Model(fullfile(Orig.Model));
+    model = Model(fullfile(SubjModel));
     model.initSystem();
-    
+
     % copy over original markerset file
-    Orig.MkrSetFile = strcat(GenericFilePath, '\',...
-        GenericDir(contains({GenericDir.name}, 'gait2392_Scale_MarkerSet_ABL_Virtual.xml')).name);
-    MkrSetFile = strcat(ScaleFolder, '\', Subjects(subj).name, '_MkrSet.xml');
+    if strcmp(Settings.CustomModel, 'Yes') % use customized model
+        Orig.MkrSetFile = strcat(GenericFilePath, '\',...
+            GenericDir(contains({GenericDir.name}, 'gait2392_Scale_MarkerSet_GT_Virtual.xml')).name);
+        MkrSetFile = strcat(ScaleFolder, '\', Subjects(subj).name, '_MkrSet.xml');
+    else
+        Orig.MkrSetFile = strcat(GenericFilePath, '\',...
+            GenericDir(contains({GenericDir.name}, 'gait2392_Scale_MarkerSet_ABL_Virtual.xml')).name);
+        MkrSetFile = strcat(ScaleFolder, '\', Subjects(subj).name, '_MkrSet.xml');
+    end
     copyfile(Orig.MkrSetFile, MkrSetFile); % copy generic scale file to subject setup directory
-    
+
     % identify setup XML file
-    Orig.ScaleSetupFile = strcat(GenericFilePath, '\', GenericDir(contains({GenericDir.name}, 'Setup_Scale')).name);
+    if strcmp(Settings.Model, 'exo')
+        Orig.ScaleSetupFile = strcat(GenericFilePath, '\', GenericDir(contains({GenericDir.name}, 'Setup_Scale_GT_Exo.xml')).name);
+    elseif strcmp(Settings.Model, 'generic')
+        Orig.ScaleSetupFile = strcat(GenericFilePath, '\', GenericDir(contains({GenericDir.name}, 'Setup_Scale_GT.xml')).name);
+    end
     [scaleXML, scaleRootName, ~] = xml_read(Orig.ScaleSetupFile);
-    
+
     % Change attributes in structure
     scaleXML.ScaleTool.mass = Subjects(subj).Demo.mass;
     scaleXML.ScaleTool.ATTRIBUTE.name = Subjects(subj).name;
-    
+
     % GenericModelMaker
-    scaleXML.ScaleTool.GenericModelMaker.model_file = 'gait2392_Scale_ABLMarkerSet_UMBprobed_locked.osim';
+    scaleXML.ScaleTool.GenericModelMaker.model_file = SubjModel;
     scaleXML.ScaleTool.GenericModelMaker.marker_set_file = strcat(Subjects(subj).name, '_MkrSet.xml');
-    
+
     % ModelScaler
     scaleXML.ScaleTool.ModelScaler.marker_file = [Subjects(subj).Trials(StaticTrials).files.OpenSimTRC(1:end-4) '_Virtual.trc'];
     OutputModelFile = strcat(Subjects(subj).name, '_Scaled.osim');
     scaleXML.ScaleTool.ModelScaler.output_model_file = OutputModelFile;
-    %     scaleXML.ScaleTool.ModelScaler.time_range = [ScaleStart ScaleEnd];
-    
+
     % MarkerPlacer
     scaleXML.ScaleTool.MarkerPlacer.marker_file = [Subjects(subj).Trials(StaticTrials).files.OpenSimTRC(1:end-4) '_Virtual.trc'];
     scaleXML.ScaleTool.MarkerPlacer.output_model_file = OutputModelFile;
-    %     scaleXML.ScaleTool.MarkerPlacer.time_range = [ScaleStart ScaleEnd];
     scaleXML.ScaleTool.MarkerPlacer.output_motion_file = strcat(Subjects(subj).name, '_Scale_Motion.sto');
     scaleXML.ScaleTool.MarkerPlacer.output_marker_file = strcat(Subjects(subj).name, '_Scale_Markers.xml');
     scaleXML.ScaleTool.MarkerPlacer.output_scale_file = strcat(Subjects(subj).name, '_ScaleSet.xml');
-    
+
     % export  XML to SetupDir (specific to each subject)
     SetupScale = strcat(ScaleFolder, '\', Subjects(subj).name, '_Setup_Scale.xml');
     xml_write(SetupScale, scaleXML, scaleRootName);
-    
-    
+
+
     %% Run Scaling
     command = ['opensim-cmd run-tool ' SetupScale];
-    system(command); 
-%     scale = ScaleTool(SetupScale); % open scaling tool with new attributes
-%     scale.run(); % run scaling
 
-            
+    delete ScaleLog.txt % clear scale log file
+    diary ScaleLog.txt; % start recording outputs
+    diary on;
+    system(command); % run scaling
+    diary off;
+
     %% save printed results in opensim log file
-%     FID = fopen('opensim.log');
-%     TXT = textscan(FID, '%s');
-%     fclose(FID);
-%     % look for each subject's model scaling
-%     for i = 1:length(TXT{1})
-%         if contains(TXT{1}(i), {'Deleted'}) &&  ...
-%                 contains(TXT{1}(i+2), {'unused'}) && contains(TXT{1}(i+3), {'markers'})...
-%                 && contains(TXT{1}(i+6), {char(Subjects(subj).name)})
-%             ind = i;
-%             break
-%         end
-%     end
-%     % extract marker error locations
-%     for i = ind:ind+50
-%         if contains(TXT{1}(i), {'total'}) && contains(TXT{1}(i+1), {'square'}) && ...
-%                 contains(TXT{1}(i+2), {'error'}) && contains(TXT{1}(i+3), {'='})
-%             Ind = i;
-%             break
-%         end
-%     end
-%     
-%     % save scale sets
-%     
-%     % save marker errors
-%     Subjects(subj).Trials(StaticTrials).ScaleErr.TotalSqErr = str2double(TXT{1}(Ind+4));
-%     Subjects(subj).Trials(StaticTrials).ScaleErr.RMSErr = str2double(TXT{1}(Ind+9));
-%     Subjects(subj).Trials(StaticTrials).ScaleErr.MaxErr = str2double(TXT{1}(Ind+12));
-%     Subjects(subj).Trials(StaticTrials).ScaleErr.MaxMkr = TXT{1}(Ind+13);
-%     
-%     disp(['Total Square Error = ' TXT{1}(Ind+4)]);
-%     disp(' ');
-    
+    disp('Analyzing Scale Error')
+    FID = fopen('ScaleLog.txt');
+    TXT = textscan(FID, '%s');
+    fclose(FID);
+    ScaleFactors = struct();
+    j = 1;
+    % look for scaling factors - for OpenSim version 4.4
+    for i = 1:length(TXT{1})
+        if contains(TXT{1}(i), {'overall'}) &&  ...
+                contains(TXT{1}(i+1), {'scale'}) && contains(TXT{1}(i+2), {'factor'})
+
+            Segment = char(TXT{1}(i-13));
+            Factor = char(TXT{1}(i+4));
+            disp([Segment ' scaled by ' Factor])
+            ScaleFactors(j).Segment = Segment;
+            ScaleFactors(j).ScaleFactor = str2double(Factor);
+            j = j + 1;
+        end
+    end
+
+    % save scale factors in matlab structure
+    Subjects(subj).Scale = ScaleFactors;
+
     clearvars ScaleRootName Geopath StaticMarkerFile NewStaticMarkerFile...
         StaticMarkerFileGRF NewStaticMarkerFileGRF i col scale Ind String
-    
+
 end % end subject loop
 
+close all;
 
 end
